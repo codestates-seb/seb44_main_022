76be548e91fb047e6c0e5adfa1f47c86045d59ca
KimJunpyo <bono1725@naver.com>
2023-07-04 14:10:44 +0900

    feat: Error Box
    1. 입력 폼 별 정규식 추가
    2. Error Box 추가
    3. Error Box에 Animation 추가

--- client/src/components/RoundButton.tsx
+++ client/src/components/RoundButton.tsx
@@ -5,17 +5,35 @@ interface ButtonProps {
   title: string;
   types: string;
   icon?: ReactElement<ComponentType>;
+  enabled?: boolean;
 }
 
 const ROUND_BUTTON_TYPE: { [index: string]: FlattenSimpleInterpolation } = {
   default: css`
-    background-color: var(--white);
+    background-color: var(--background);
     color: var(--dark-blue-black);
     font-weight: 700;
+    &:hover {
+      background-color: var(--gray);
+    }
+    &:active {
+      background-color: var(--normal-gray);
+    }
   `,
   dark: css`
     background-color: var(--dark-gray);
     color: var(--white);
+    &:hover {
+      background-color: var(--white-gray);
+    }
+    &:active {
+      background-color: var(--bright-black);
+    }
+    :disabled {
+      background-color: var(--dark-gray);
+      color: var(--white);
+      opacity: 0.6;
+    }
   `,
   purple: css`
     background-color: var(--purple);
@@ -23,9 +41,9 @@ const ROUND_BUTTON_TYPE: { [index: string]: FlattenSimpleInterpolation } = {
   `,
 };
 
-function RoundButton({ title, types, icon }: ButtonProps) {
+function RoundButton({ title, types, icon, enabled }: ButtonProps) {
   return (
-    <RoundButtonStyle types={types}>
+    <RoundButtonStyle types={types} disabled={enabled === false && true}>
       {icon && <Icons>{icon}</Icons>}
       {title}
     </RoundButtonStyle>
@@ -42,6 +60,8 @@ const RoundButtonStyle = styled.button<{ types: string }>`
   border: 2px solid var(--normal-gray);
   border-radius: 100px;
   align-items: center;
+  outline: none;
+  transition: 0.3s;
   ${({ types }) => ROUND_BUTTON_TYPE[types] || ROUND_BUTTON_TYPE.default};
 `;
 
--- client/src/pages/users/SignUpForm.tsx
+++ client/src/pages/users/SignUpForm.tsx
@@ -2,38 +2,80 @@ import { MdLocalPostOffice } from 'react-icons/md';
 import { FaUserCircle } from 'react-icons/fa';
 import UserInput from '../../components/UserInput';
 import { AiFillLock } from 'react-icons/ai';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RoundButton from '../../components/RoundButton';
+import { AUTH_FAILED_MESSAGE, REGEX } from '../../assets/constantValue/constantValue';
 
 function SignUpForm() {
   const [nickname, setNickname] = useState<string>('');
   const [userId, setUserId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [userIdValid, setUserIdValid] = useState<boolean>(false);
+  const [passwordValid, setPasswordValid] = useState<boolean>(false);
+  const [nicknameValid, setNicknameValid] = useState<boolean>(false);
+
+  const handleLoginSubmit: React.FormEventHandler<HTMLElement> = (e) => {
+    e.preventDefault();
+    if (userIdValid && passwordValid) {
+      // 통신 코드 예정
+      console.log(nickname, userId, password);
+      alert('통신 성공');
+      return;
+    }
+  };
+
+  const strCheck = (str: string, type: string) => {
+    if (type === 'id') {
+      return REGEX.id.test(str);
+    }
+    if (type === 'password') {
+      return REGEX.password.test(str);
+    }
+    return REGEX.nickname.test(str);
+  };
+
+  useEffect(() => {
+    setNicknameValid(strCheck(nickname, 'nickname'));
+  }, [nickname]);
+
+  useEffect(() => {
+    setUserIdValid(strCheck(userId, 'id'));
+  }, [userId]);
+
+  useEffect(() => {
+    setPasswordValid(strCheck(password, 'password'));
+  }, [password]);
 
   return (
-    <form>
+    <form onSubmit={handleLoginSubmit}>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <UserInput
           setState={setNickname}
           state={nickname}
           placeholderText="Nickname"
+          dataValid={nicknameValid}
+          vaildMessage={AUTH_FAILED_MESSAGE.nickname}
           icon={<FaUserCircle />}
         />
         <UserInput
           setState={setUserId}
           state={userId}
           placeholderText="ID"
+          dataValid={userIdValid}
+          vaildMessage={AUTH_FAILED_MESSAGE.id}
           icon={<MdLocalPostOffice />}
         />
         <UserInput
           setState={setPassword}
           state={password}
           placeholderText="Password"
+          dataValid={passwordValid}
+          vaildMessage={AUTH_FAILED_MESSAGE.password}
           icon={<AiFillLock />}
         />
       </div>
       <div style={{ marginTop: '2.5rem' }}>
-        <RoundButton title="회원가입" types="dark" />
+        <RoundButton title="회원가입" types="dark" enabled={userIdValid && passwordValid} />
         <RoundButton title="사업자 회원이신가요?" types="purple" />
       </div>
     </form>
