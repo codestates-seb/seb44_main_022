fecbd58197a872735a3715d26b5d0d8cc87904a4
jungo <81670100+jungo0@users.noreply.github.com>
2023-07-06 18:20:37 +0900

    fix: 오류 수정

--- client/src/share/ModalComponentCustom.tsx
+++ client/src/share/ModalComponentCustom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
 import Draggable from 'react-draggable';
