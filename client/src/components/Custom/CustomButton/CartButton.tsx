427c81cca87f9f5cb5bd15ac74d37a874feb5511
jungo <81670100+jungo0@users.noreply.github.com>
2023-07-09 19:53:09 +0900

    feat: 커스텀버튼 컴포넌트 분리

---
+++ client/src/components/Custom/CustomButton/CartButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import modal_cart from '../../../assets/images/img_modal/modal_cart.png';
+
+const CartButtonContainer = styled.button`
+  position: absolute;
+  width: 115px;
+  height: 75px;
+  bottom: 0px;
+  right: 0px;
+  padding: 10px;
+  border: none;
+  background-color: transparent;
+  border: 1px solid var(--light-black);
+  border-radius: 0px 0px 20px 0px;
+  background-color: rgba(20, 46, 56, 0.9);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CartImage = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-bottom: 8px;
+`;
+
+const CartButtonText = styled.span`
+  color: var(--white);
+  font-size: 12px;
+`;
+
+type CartButtonProps = {
+  onRequestClose: () => void;
+};
+
+const CartButtonComponent: React.FC<CartButtonProps> = ({ onRequestClose }) => {
+  return (
+    <CartButtonContainer onClick={onRequestClose}>
+      <CartImage src={modal_cart} alt="Cart" />
+      <CartButtonText>장바구니 담기</CartButtonText>
+    </CartButtonContainer>
+  );
+};
+
+export default CartButtonComponent;
--- client/src/components/Custom/CustomButton/ModalButtons.tsx
+++ client/src/components/Custom/CustomButton/ModalButtons.tsx
@@ -1,48 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import modal_cart from '../assets/images/img_modal/modal_cart.png';
-import modal_cake from '../assets/images/img_modal/modal_cake.png';
-const CloseButton = styled.button`
-  position: absolute;
-  width: 70px;
-  height: 50px;
-  top: 0px;
-  right: 0px;
-  padding: 10px;
-  border: none;
-  background-color: rgba(20, 46, 56, 0.9);
-  color: white;
-  border-radius: 0px 20px 0px 0px;
-`;
-
-const CartButton = styled.button`
-  position: absolute;
-  width: 115px;
-  height: 75px;
-  bottom: 0px;
-  right: 0px;
-  padding: 10px;
-  border: none;
-  background-color: transparent;
-  border: 1px solid var(--light-black);
-  border-radius: 0px 0px 20px 0px;
-  background-color: rgba(20, 46, 56, 0.9);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const CartImage = styled.img`
-  width: 30px;
-  height: 30px;
-  margin-bottom: 8px;
-`;
-
-const CartButtonText = styled.span`
-  color: var(--white);
-  font-size: 6px;
-`;
+import modal_cake from '../../../assets/images/img_modal/modal_cake.png';
+import CloseButton from './CloseButton';
+import CartButton from './CartButton';
 
 const ImageBox = styled.div`
   position: absolute;
@@ -69,14 +29,11 @@ type ModalButtonsProps = {
 const ModalButtons: React.FC<ModalButtonsProps> = ({ onRequestClose }) => {
   return (
     <>
-      <CloseButton onClick={onRequestClose}>X</CloseButton>
+      <CloseButton onClick={onRequestClose} />
       <ImageBox>
         <Image src={modal_cake} alt="Cart" />
       </ImageBox>
-      <CartButton>
-        <CartImage src={modal_cart} alt="Cart" />
-        <CartButtonText>장바구니 담기</CartButtonText>
-      </CartButton>
+      <CartButton onRequestClose={onRequestClose} />
     </>
   );
 };
