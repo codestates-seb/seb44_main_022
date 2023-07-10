e52c4fcfb6adccda2ccda801dadeb54d5f3b0109
ljg980708 <ljg980708@gmail.com>
2023-07-10 10:16:22 +0900

    refactor: Refactor service class

--- server/src/main/java/com/buyte/product/service/ProductService.java
+++ server/src/main/java/com/buyte/product/service/ProductService.java
@@ -1,95 +1,13 @@
 package com.buyte.product.service;
 
-import com.buyte.exception.BusinessLogicException;
-import com.buyte.exception.ExceptionCode;
-import com.buyte.ingredient.dto.AllergyIngredientDto;
-import com.buyte.ingredient.dto.IngredientOriginDto;
-import com.buyte.ingredient.entity.Ingredient.AllergyIngredient;
-import com.buyte.ingredient.mapper.IngredientMapper;
-import com.buyte.product.dto.PreferenceProductDto;
 import com.buyte.product.dto.PreferenceProductPageDto;
 import com.buyte.product.dto.ProductDetailsDto;
 import com.buyte.product.entity.Product;
-import com.buyte.product.entity.Product.PreferenceProduct;
-import com.buyte.product.mapper.ProductMapper;
-import com.buyte.product.repository.ProductRepository;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Slf4j
-@Transactional
-public class ProductService {
+public interface ProductService {
+    PreferenceProductPageDto getPreferenceProducts(int page);
 
-    private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
-    private final IngredientMapper ingredientMapper;
+    ProductDetailsDto getProductDetails(long storeId, long productId);
 
-    public ProductService(ProductRepository productRepository, ProductMapper productMapper,
-        IngredientMapper ingredientMapper) {
-        this.productRepository = productRepository;
-        this.productMapper = productMapper;
-        this.ingredientMapper = ingredientMapper;
-    }
-
-    @Transactional(readOnly = true)
-    public PreferenceProductPageDto getPreferenceProducts(int page) {
-
-        Pageable pageable = PageRequest.of(page, 20, Sort.by("createdAt").descending());
-
-        Page<Product> findProductPage = productRepository.findByPreferenceProduct(PreferenceProduct.PREFERRED, pageable);
-
-        List<PreferenceProductDto> preferenceProductList = findProductPage.getContent().stream()
-            .map(product -> productMapper.productToPreferenceProduct(product))
-            .collect(Collectors.toList());
-
-        PreferenceProductPageDto preferenceProductPageDto = productMapper.productPageToPreferenceProductPage(findProductPage);
-
-        preferenceProductPageDto.setPreferenceProductList(preferenceProductList);
-
-        return preferenceProductPageDto;
-    }
-
-    @Transactional(readOnly = true)
-    public ProductDetailsDto getProductDetails(long productId) {
-        Product findProduct = findProduct(productId);
-
-        List<IngredientOriginDto> ingredientOriginList = findProduct.getProductIngerdientList().stream()
-            .map(productIngerdient -> productIngerdient.getIngredient())
-            .map(ingredient -> ingredientMapper.ingredientToIngredientOrigin(ingredient))
-            .collect(Collectors.toList());
-
-        List<AllergyIngredientDto> allergyIngredientList = findProduct.getProductIngerdientList().stream()
-            .map(productIngerdient -> productIngerdient.getIngredient())
-            .filter(ingredient -> ingredient.getAllergyIngredient() == AllergyIngredient.ALLERGIC)
-            .map(ingredient -> ingredientMapper.ingredientToAllergyIngredient(ingredient))
-            .collect(Collectors.toList());
-
-        ProductDetailsDto productDetailsDto = productMapper.productToProductDetails(findProduct);
-        productDetailsDto.setIngredientOriginList(ingredientOriginList);
-        productDetailsDto.setAllergyIngredientList(allergyIngredientList);
-
-        return productDetailsDto;
-    }
-
-    @Transactional(readOnly = true)
-    public Product findProduct(long productId) {
-        return findVerifiedProduct(productId);
-    }
-
-    @Transactional(readOnly = true)
-    public Product findVerifiedProduct(long productId) {
-        Optional<Product> optionalProduct = productRepository.findById(productId);
-        Product findProduct = optionalProduct.orElseThrow(
-            () -> new BusinessLogicException(ExceptionCode.PRODUCT_NOT_FOUND));
-        return findProduct;
-    }
+    Product findVerifiedProduct(long productId);
 }
--- server/src/main/java/com/buyte/store/service/StoreService.java
+++ server/src/main/java/com/buyte/store/service/StoreService.java
@@ -1,147 +1,18 @@
 package com.buyte.store.service;
 
-import com.buyte.dto.PageInfoDto;
-import com.buyte.exception.BusinessLogicException;
-import com.buyte.exception.ExceptionCode;
-import com.buyte.product.dto.CustomProductInfoDto;
-import com.buyte.product.dto.ProductInfoDto;
-import com.buyte.product.dto.StandardProductInfoDto;
-import com.buyte.product.entity.Product;
-import com.buyte.product.entity.Product.PreferenceProduct;
-import com.buyte.product.entity.Product.ProductType;
-import com.buyte.product.mapper.ProductMapper;
 import com.buyte.store.dto.StoreDetailsDto;
-import com.buyte.store.dto.StoreInfoDto;
 import com.buyte.store.dto.StoreInfoPageDto;
 import com.buyte.store.dto.StoreMapDto;
 import com.buyte.store.entity.Store;
-import com.buyte.store.mapper.StoreMapper;
-import com.buyte.store.repository.StoreRepository;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Slf4j
-@Transactional
-public class StoreService {
+public interface StoreService {
 
-    private final StoreRepository storeRepository;
-    private final StoreMapper storeMapper;
-    private final ProductMapper productMapper;
+    StoreInfoPageDto getStores(int page, String search);
 
-    public StoreService(StoreRepository storeRepository, StoreMapper storeMapper,
-        ProductMapper productMapper) {
-        this.storeRepository = storeRepository;
-        this.storeMapper = storeMapper;
-        this.productMapper = productMapper;
-    }
+    List<StoreMapDto> getStoreMap();
 
-    @Transactional(readOnly = true)
-    public StoreInfoPageDto getStores(int page, String search) {
+    StoreDetailsDto getStoreDetails(long storeId);
 
-        log.info("# storeName : " + search);
-
-        Page<Store> findStorePage;
-
-        Pageable pageable = PageRequest.of(page, 20, Sort.by("createdAt").descending());
-
-        if (search != null) {
-            findStorePage = storeRepository.findByStoreNameContaining(search, pageable);
-        } else {
-            findStorePage = storeRepository.findAll(pageable);
-        }
-
-        List<StoreInfoDto> storeInfoList = findStorePage.getContent().stream()
-            .map(store -> storeMapper.storeToStoreInfo(store))
-            .collect(Collectors.toList());
-
-        StoreInfoPageDto storeInfoPageDto = storeMapper.storePageToStoreInfoPage(findStorePage);
-
-        storeInfoPageDto.setStoreInfoList(storeInfoList);
-
-        return storeInfoPageDto;
-    }
-
-    @Transactional(readOnly = true)
-    public List<StoreMapDto> getStoreMap() {
-
-        List<Store> allStoreList = storeRepository.findAll();
-
-        List<StoreMapDto> storeMapList = new ArrayList<>();
-
-        allStoreList.forEach(store -> {
-            StoreMapDto storeMapDto = storeMapper.storeToStoreMap(store);
-
-            List<ProductInfoDto> productInfoList = store.getProductList().stream()
-                .filter(product -> product.getPreferenceProduct() == PreferenceProduct.PREFERRED)
-                .map(productMapper::productToProductInfo).collect(Collectors.toList());
-
-            storeMapDto.setProductPreferenceList(productInfoList);
-            storeMapList.add(storeMapDto);
-        });
-
-        return storeMapList;
-    }
-
-
-    @Transactional(readOnly = true)
-    public StoreDetailsDto getStoreDetails(long storeId) {
-
-        log.info("# sotreId : " + storeId);
-
-        Store findStore = findStore(storeId);
-
-        List<Product> productList = findStore.getProductList();
-
-        StoreDetailsDto storeDetailsDto = storeMapper.storeToStoreDetails(findStore);
-
-//        List<CustomProductInfoDto> customProductInfoList = productList.stream()
-//            .filter(product -> product.getProductType() == ProductType.CUSTOM)
-//            .map(product -> productMapper.productToCustomProductInfo(product))
-//            .collect(Collectors.toList());
-//
-//        List<StandardProductInfoDto> standardProductInfoList = productList.stream()
-//            .filter(product -> product.getProductType() == ProductType.STANDARD)
-//            .map(product -> productMapper.productToStandardProductInfo(product))
-//            .collect(Collectors.toList());
-
-        List<CustomProductInfoDto> customProductInfoList = new ArrayList<>();
-        List<StandardProductInfoDto> standardProductInfoList = new ArrayList<>();
-
-        productList.stream().forEach(product -> {
-            if (product.getProductType() == ProductType.CUSTOM) {
-                customProductInfoList.add(productMapper.productToCustomProductInfo(product));
-            } else if (product.getProductType() == ProductType.STANDARD) {
-                standardProductInfoList.add(productMapper.productToStandardProductInfo(product));
-            }
-        });
-
-        storeDetailsDto.setCustomProductInfoList(customProductInfoList);
-        storeDetailsDto.setStandardProductInfoList(standardProductInfoList);
-
-        return storeDetailsDto;
-    }
-
-    @Transactional(readOnly = true)
-    public Store findStore(long storeId) {
-        return findVerifiedStore(storeId);
-    }
-
-    @Transactional(readOnly = true)
-    public Store findVerifiedStore(long storeId) {
-        Optional<Store> optionalStore = storeRepository.findById(storeId);
-        Store findStore = optionalStore.orElseThrow(
-            () -> new BusinessLogicException(ExceptionCode.STORE_NOT_FOUND));
-        return findStore;
-    }
+    Store findVerifiedStore(long storeId);
 }
