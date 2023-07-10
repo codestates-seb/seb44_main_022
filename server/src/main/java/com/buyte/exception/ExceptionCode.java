2c2f1eea21f49d166d409e1873846f8cb85dbd5e
ljg980708 <ljg980708@gmail.com>
2023-07-10 10:34:24 +0900

    feat: Add exceptioncode

--- server/src/main/java/com/buyte/exception/ExceptionCode.java
+++ server/src/main/java/com/buyte/exception/ExceptionCode.java
@@ -6,7 +6,10 @@ public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    STORE_NOT_FOUND(404, "Store not found");
+    STORE_NOT_FOUND(404, "Store not found"),
+    PRODUCT_NOT_FOUND(404, "Product not found"),
+    PRODUCT_TYPE_CUSTOM_FORBIDDEN(403, "Access to product details for custom products is forbidden");
+
 
     @Getter
     private int status;
