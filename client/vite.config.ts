22e561201a55f32a3f8e148607185a5c2122c9f4
KimJunpyo <bono1725@naver.com>
2023-07-11 10:43:58 +0900

    feat: build 2
    1. postCss-Nesting 관련 문제 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['chrome87', 'edge88', 'es2020', 'firefox78', 'safari14'],
+        }),
+        postcssNesting(),
+      ],
+    },
+  },
+});
