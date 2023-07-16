f2bfeda86dd87826190be1c1981f6adf5431c80a
Raros17 <sookmeoyhj@gmail.com>
2023-07-16 18:52:09 +0900

    feat: mypage layout

--- client/src/components/Custom/CustomContent/CustomContent.tsx
+++ client/src/components/Custom/CustomContent/CustomContent.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import ColorInput from './ColorInput';
 import EraseButton from './EraseButton';
--- client/src/pages/mypage/Mypage.tsx
+++ client/src/pages/mypage/Mypage.tsx
@@ -1,5 +1,52 @@
+import styled from 'styled-components';
+import CartItemTab from '../../components/CartItem/CartItemTab';
 function Mypage() {
-  return <div style={{ marginTop: '160px' }}></div>;
-}
+  return <div style={{ marginTop: '160px', display: 'flex', justifyContent:'center' }}>
+    <MyPageWrapper>
+      <p style={{fontWeight:'800', color: 'var(--dark-gray)', textAlign:'center'}}>안녕하세요, <span style={{color: 'var(--purple)'}}>nickname</span>님!</p>
+      <MyInfoSection>
+        <div style={{width: '200px', height: '180px', backgroundColor:'gray', paddingRight:'10px'}}></div>
+        <MyInfoDetail>
+          <h3>nickName</h3>
+          <span style={{fontSize:'13px', color:'var(--purple)', cursor:'pointer'}}>회원 정보 수정</span>
+        </MyInfoDetail>
+      </MyInfoSection>   
+      <MyOrderSection>
+        <h2 style={{fontWeight:'800', fontSize:'18px', color:'var(--light-gray)', marginBottom:'10px'}}>나의 주문</h2>
+        <p style={{fontWeight:'500'}}>주문내역</p>
+       <MyOrderDetail>
+        <CartItemTab/>
+        <p>주문내역이 없습니다.</p>
+      </MyOrderDetail>
+      </MyOrderSection>
+    </MyPageWrapper>
 
+  </div>;
+}
 export default Mypage;
+
+const MyPageWrapper = styled.section`
+  width: 70%;
+`
+const MyInfoSection = styled.section`
+  width: 100%;
+  display: flex;
+  padding: 40px 30px;
+  background-color: aliceblue;
+`
+const MyInfoDetail = styled.section`
+  margin-left: 30px;
+  h3 {
+    color: var(--light-gray);
+    margin-bottom: 18px;
+    font-size: 18px;
+    font-weight: 800;
+  }
+`
+const MyOrderSection= styled.section`
+  padding: 20px;
+  background-color: aquamarine;
+`
+const MyOrderDetail= styled.section`
+padding: 20px;
+`
