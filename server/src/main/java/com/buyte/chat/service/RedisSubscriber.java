76d01d154d8c505c395dcbc7224ac59ed4e40f2b
j6d9s <qxdo19@gmail.com>
2023-07-23 01:00:02 +0900

    feat: redis sub/pub

---
+++ server/src/main/java/com/buyte/chat/service/RedisSubscriber.java
@@ -0,0 +1,35 @@
+package com.buyte.chat.service;
+
+import com.buyte.chat.dto.RedisChat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisSubscriber implements MessageListener {
+
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String,Object> redisTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            //레디스 에서 받은 데이터를 역직렬화
+            String publishMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+            RedisChat redisChat = objectMapper.readValue(publishMessage, RedisChat.class);
+            messagingTemplate.convertAndSend("/sub/" + redisChat.getRoomId(), redisChat);
+            log.info("레디스 메시지 수신");
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+        }
+    }
+}
